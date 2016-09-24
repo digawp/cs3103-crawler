d963db7b6d001420344df9737f5a000a13c8dc84
Diga Widyaprana <digawp@gmail.com>
2016-09-24 13:00:38 +0800

    Don't exit if minor errors happen, minor bugfixes and improvements

--- crawler.cpp
+++ crawler.cpp
@@ -17,22 +17,21 @@ typedef struct Url Url;
 Crawler::Crawler(Storage& store) : storage(store) {}
 
 void Crawler::run() {
-    Url url = storage.get_next_url();
-
     while (true) {
+        Url url = storage.get_next_url();
         int socket_desc;
         struct addrinfo* addrinfo_res;
 
         int addrinfo_status = getaddrinfo(url.base.c_str(), "http", NULL, &addrinfo_res);
         if (addrinfo_status != 0) {
             perror(gai_strerror(addrinfo_status));
-            return;
+            continue;
         }
 
         bool connect_success = connect(addrinfo_res, &socket_desc);
         if (!connect_success) {
             perror("Cannot connect to any of the results");
-            return;
+            continue;
         }
         freeaddrinfo(addrinfo_res);
 
@@ -53,7 +52,7 @@ void Crawler::run() {
         // In case the response terminated early
         if (bytes_read == -1) {
             perror("recv");
-            return;
+            continue;
         }
 
         HttpResponse response = parse_response(ss);
@@ -69,44 +68,42 @@ void Crawler::run() {
         std::chrono::microseconds duration =
             std::chrono::duration_cast<std::chrono::microseconds>(end - start);
         double elapsed = duration.count()/1000.0;
-        std::cout << elapsed << std::endl;
-        storage.report_res_time(url.full(), elapsed);
+        std::cout << url.full() << "\t" << elapsed << std::endl;
+        storage.report_res_time(url.base, elapsed);
 
         close(socket_desc);
 
         // TODO remove this to run indefinitely
-        break;
+        // break;
     } // end of while
 }
 
 bool Crawler::connect(struct addrinfo* ai_results, int* socket_desc) {
     struct addrinfo* rp;
 
-    for (rp = ai_results; rp != NULL; rp = rp->ai_next)
-    {
+    for (rp = ai_results; rp != NULL; rp = rp->ai_next) {
         struct sockaddr_in* addr;
         addr = (struct sockaddr_in *)rp->ai_addr;
 
         *socket_desc =
             socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if (*socket_desc == -1)
-        {
+        if (*socket_desc == -1) {
             perror("client: socket");
             continue;
         }
+
         int connect_status =
             ::connect(*socket_desc, rp->ai_addr, rp->ai_addrlen);
-        if (connect_status < 0)
-        {
+        if (connect_status < 0) {
             close(*socket_desc);
             perror("client: connect");
             continue;
         }
-        break; // connect successful or ran out of rp
+        break; // connect successful
     }
 
-    if (rp == NULL)
-    {
+    if (rp == NULL) {
+        // ran out of rp
         return false;
     }
     return true;
@@ -116,7 +113,7 @@ std::string Crawler::construct_req_header(const std::string& host, const std::st
     std::ostringstream oss;
     oss << "GET " << path << " HTTP/1.0\r\n";
     oss << "Host: " << host << "\r\n";
-    oss << "Accept: */*" << "\r\n" << "\r\n";
+    oss << "Accept: */*" << "\r\n\r\n";
     // oss << "Connection: close" << "\r\n" << "\r\n";
     return oss.str();
 }
